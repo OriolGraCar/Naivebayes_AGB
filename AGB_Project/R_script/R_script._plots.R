7f80a629c3a8d9d1ed7eaed609b7a84900e40b3e
helenarodllo <33523509+helenarodllo@users.noreply.github.com>
2018-02-28 02:21:00 +0100

    Add files via upload

--- AGB_Project/R_script/R_script._plots.R
+++ AGB_Project/R_script/R_script._plots.R
@@ -18,32 +18,25 @@ plot(x, y, type="h", main="NA counts ratio", xlab = "NA ratio", ylab = "Percent 
 
 ### % correct per quant of attributes############
 # quants encertem amb un attribut, quants amb 2, quants amb 3.... -> GRAPH DE UNA SOLA LINIA CURVA
-dd <- read.table("/home/helena/Escriptori/2nd_term/AGB/project/NA_tissue_sum.txt", header = T, sep = "\t", dec = ".")#loads the file with the NA ratio accumuative count 
+dd <- read.table("/home/helena/Escriptori/2nd_term/AGB/project/ratio_counts.txt", header = T, sep = "\t", dec = ".")#loads the file with the NA ratio accumuative count 
 
 x <- dd$ratio
 y<- dd$counts
 
-y <- (y/37827)*100 
+#y <- (y/37827)*100 
 
-plot(x, y, type="p", main="Percent of correct predictions per attribute", xlab = "Number of attributes", ylab = "Percent of correct predictions",
-     #col="mediumseagreen",
-     col="chocolate1"
+plot(x, y, type="o", main="Percent of correct predictions per attribute", xlab = "Number of attributes", ylab = "Percent of correct predictions",
+     col="mediumseagreen"
+     #col="chocolate1"
 ) #plotting the line with the results.
 
 ##### PLOT correct versus fail##############
 #dat <-read.table(text = " tissue tissue tissue
 #                Correct 15 252 333
 #               Incorrect 857 525 847", sep = "", header=TRUE)
-dat <-read.table("/home/helena/Escriptori/2nd_term/AGB/project/NA_tissue_sum.txt", header = T, sep = "\t", dec = ".")
-
-datm <- melt(cbind(dat, ind = rownames(dat)), id.vars = c('ind'))
-
-ggplot(datm,aes(x = variable, y = value, fill = ind)) + 
-  geom_bar(position = "fill",stat = "identity") +
-  scale_y_continuous(labels = percent_format())
-
-##### 10-FOLD graphic #####
-dat <-read.table("/home/helena/Escriptori/2nd_term/AGB/project/NA_tissue_sum.txt", header = T, sep = "\t", dec = ".")
+dat <-read.table(text = "Cerebellum	Cerebellar_Hemisphere	Cingulate_Cortex	Cortex	Hippocampus	Amygdala	Caudate	Hypothalamus	Nucleus_Accumben	Frontal_Cortex	Putamen	Spinal_Cord	Substantia_Nigra
+                  Correct	55	35	4	32	12	1	33	8	30	28	6	4	1
+                  Incorrect	9	7	24	18	17	13	21	19	19	19	20	1	1", header = T, sep = "\t", dec = ".")
 
 datm <- melt(cbind(dat, ind = rownames(dat)), id.vars = c('ind'))
 
@@ -52,30 +45,34 @@ ggplot(datm,aes(x = variable, y = value, fill = ind)) +
   scale_y_continuous(labels = percent_format())
 
 ##### COMPARISON with and without pseudocounts####
-dat <-read.table("/home/helena/Escriptori/2nd_term/AGB/project/NA_tissue_sum.txt", header = T, sep = "\t", dec = ".")
+dat <-read.table(text = "ratio	counts
+                          0.562929061784897	p
+                          0.5697940503432495	m", header = T, sep = "\t", dec = ".")
 
-datm <- melt(cbind(dat, ind = rownames(dat)), id.vars = c('ind'))
+barplot(dat$ratio, main="Correct ratio", horiz=TRUE, 
+        names.arg=c("No-pseudocounts", "Pseudocounts"),col="mediumseagreen",xlim = c(0,0.6))
 
-ggplot(datm,aes(x = variable, y = value, fill = ind)) + 
-  geom_bar(position = "fill",stat = "identity") +
-  scale_y_continuous(labels = percent_format())
+### Redundancy ######
+dat <-read.table(text = "ratio	counts
+                          0.47368421052631576	r
+                          0.5697940503432495	m", header = T, sep = "\t", dec = ".")
 
-
+barplot(dat$ratio, main="Correct ratio", horiz=TRUE, 
+        names.arg=c("Redundancy", "No-redundancy"),col="mediumseagreen",xlim = c(0,0.6))
 
 
 
 
 ### ROC curve####
 # x=y i una linia per cada teixit(color diferent cada un). 
-#test <- read.table(text= "FPR	TPR	Tissue	Tr
-#                          1	3	hola	2
- #                         2	5	hola	3
-  #                        3	8	hola	5
-   #                       1	3	adeu	3
-    #                      3	5	adeu	2
-     #                     5	7	adeu	2", sep="\t", header=TRUE)
+test<-read.table("/home/helena/Escriptori/2nd_term/AGB/project/ROC_data_plot.txt", header = T, sep = "\t", dec = ".")#loads the file with the NA ratio accumuative count 
+
 x<-test$FPR
 y<-test$TPR
-xyplot(x~y,type="o",groups= Tissue,data=test,auto.key=T)
+pdf("./ROC_brain.pdf")
+xyplot(y~x,groups= Tissue,data=test,auto.key=T, col=rainbow(14), type="p",
+       xlab="FPR",ylab = "TPR",main="ROC curve for brain tissues"
 
+)
+dev.off()
 
